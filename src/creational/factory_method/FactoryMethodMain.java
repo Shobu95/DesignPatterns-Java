8e127c48d01e718e3d548f3ec7f55c2062241463
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2021-12-14 21:54:47 +0500

    fixed main of factory-method

--- src/creational/factory_method/FactoryMethodMain.java
+++ src/creational/factory_method/FactoryMethodMain.java
@@ -5,19 +5,19 @@ public class FactoryMethodMain {
         VehicleFactory shapeFactory = new VehicleFactory();
 
         //get an object of Circle and call its draw method.
-        Vehicle vehicle1 = shapeFactory.getVehicle("CIRCLE");
+        Vehicle vehicle1 = shapeFactory.getVehicle("CAR");
 
         //call draw method of Circle
         vehicle1.showWheels();
 
         //get an object of Rectangle and call its draw method.
-        Vehicle vehicle2 = shapeFactory.getVehicle("RECTANGLE");
+        Vehicle vehicle2 = shapeFactory.getVehicle("RICKSHAW");
 
         //call draw method of Rectangle
         vehicle2.showWheels();
 
         //get an object of Square and call its draw method.
-        Vehicle vehicle3 = shapeFactory.getVehicle("SQUARE");
+        Vehicle vehicle3 = shapeFactory.getVehicle("BIKE");
 
         //call draw method of square
         vehicle3.showWheels();
